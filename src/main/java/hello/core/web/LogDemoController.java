dc9449e3b223a16fb5583bd0f35e99bf00a5ffad
JiEunKIM <jieun.kim@emotion.co.kr>
2022-08-24 18:03:34 +0900

    2022-08-22 spring-boot 5

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,37 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+  private final LogDemoService logDemoService;
+  private final ObjectProvider<MyLogger> myLoggerProvider;
+//  private final MyLogger myLogger;
+
+  //spring interceptor
+  @RequestMapping("log-demo")
+  @ResponseBody
+  public String logDemo(HttpServletRequest request) {
+    //요기서 스코프 요청
+    //생성되는 시점
+    MyLogger myLogger = myLoggerProvider.getObject();
+    String requestURL = request.getRequestURL().toString();
+    myLogger.setRequestURL(requestURL);
+
+    myLogger.log("controller test");
+    logDemoService.logic("testId");
+
+    return "OK";
+  }
+}
